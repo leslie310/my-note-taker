0f1b3d6af0fa1901889eaca2066c576ab05715b6
Leslie Saucedo <lsaucedo310@gmail.com>
2022-08-31 19:48:37 -0700

    commit

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,31 +1,46 @@
 const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
-const app =express();
+const uuid = require('../public/assets/helpers/uuid');
+const router = express.Router();
 
 
-app.get('api/notes',(req, res)=> {
-    console.log('GET note req');
-
-    let data= JSON.parse(fs.readFileSync('./db/db.json'));
-    console.log('GET req - return notes data:' = JSON.stringify(data));
-
-    response.json(data);
+router.get('/',(req,res) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) {
+            throw err;
+        } else {
+            const notes = JSON.parse(data);
+            res.json(notes);
+        }
+    });
 });
 
-app.post('/api/notes',(req,res)=> {
 
-    const newNote = request.body;
-
-    console.log('post request - New note :' + JSON.stringify(newNote));
-
-    newNote.id = uuidv4();
-    let data= JSON.parse(fs.readFileSync('./db/db.json'));
-    data.push(newNote);
-    
-    fs.writeFileSync('./db/db.json',JSON.stringify(data));
-    console.log('new note added!');
-    response.json(data);
-    
+router.post('/', (req,res) => {
+    const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuid()
+    }
+fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    if(err) {
+        throw err;
+    } else {
+        const notes = JSON.parse(data);
+        notes.push(newNote);
+        fs.writeFile(
+            './db/db.json',
+            JSON.stringify(notes, null, 4),
+            (err, data) => {
+                if(err) {
+                    throw err;
+                }
+                res.json({data: req.body, message: "success!"});
+            }
+        );
+    }
+});
 });
 
-module.exports = app;
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('/routes/htmlRoutes');
+const path = require('path');
+const api = require('./routes/apiRoutes.js');
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app,use('/', htmlRoutes);
+app.use('/api/apiRoutes', api);
 
+
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 app.listen(PORT, () => {
   console.log(`Api now installed ${PORT}`);
 });
